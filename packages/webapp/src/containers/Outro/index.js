932493e194ab02833047fd33d3627e3de7d92d90
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-10-26 11:25:50 -0700

    Implemented UI

--- packages/webapp/src/containers/Outro/index.js
+++ packages/webapp/src/containers/Outro/index.js
@@ -1,6 +1,8 @@
 import OutroImg from '../../assets/images/outro/outro.svg';
 import React, {Component} from "react";
 import styles from "./styles.scss";
+import Footer from '../../stories/Footer/index';
+import Button from '../../stories/Button/index'
 
 class Outro extends Component {
     constructor(props) {
@@ -13,12 +15,21 @@ class Outro extends Component {
                 <div className={styles.lander}>
                     <div>
                         <div className={styles.greetContainer}>
-                        <img src={OutroImg} alt=""/>
-                            <h3>{`And finally, let us show you a couple of important things!`}</h3>
+                        <img src={OutroImg}/>
+                            <div className={styles.description}> 
+                                <h3>{`And finally, let us show you a couple of important things!`}</h3>
+                            </div>
+                        </div>
+                    
+                        <Footer
+                            children={<><Button fullLength color="secondary" children='Go back'/><Button fullLength children='Finish'/></>}
+                        >
+
+                       
+                        </Footer>
                         </div>
                     </div>
                 </div>
-            </div>
         )
     }
 }
