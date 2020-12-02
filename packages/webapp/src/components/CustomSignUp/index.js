652dc2cd1eed1091e690e4836877d7ee70b47362
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-12-01 16:53:28 -0800

    Remove commented out code

--- packages/webapp/src/components/CustomSignUp/index.js
+++ packages/webapp/src/components/CustomSignUp/index.js
@@ -9,37 +9,6 @@ import Footer from '../Footer';
 export default function PureCustomSignUp({ ssoSignUp, onContinue }) {
 
   return (
-    // <>
-    // <div className={styles.home}>
-    //   <div className={styles.lander}>
-    //     <div className={styles.logo}>
-    //       <img src={Logo}/>
-    //     </div>
-    //     <div className={styles.sso}>
-    //       <Button fullLength children='CONTINUE WITH GOOGLE' color='secondary' onClick={ssoSignUp} />
-    //     </div>
-    //     <div className={styles.lineBreak}>
-    //       <img src={LineBreak}/>
-    //     </div>
-    //     <div className={styles.manualInput}>
-    //       <Input className={styles.manualInputContainer} label='Enter your email' />
-    //     </div>
-    //     </div>
-    // </div>
-    //   {/* <Footer className={styles.buttonContainer} style={{ position: 'sticky', bottom: '0' }}
-    //             children={<><Button className={styles.button} fullLength color="secondary" children='Continue' onClick={onContinue}/>
-    //             </>}
-    //     >
-    //   </Footer> */}
-    //    <Footer style={{ position: 'sticky', bottom: '0' }}
-    //           children={<><Button fullLength color="secondary" children='Go back' onClick={ssoSignUp}/>
-
-    //             <Button fullLength children='Finish' onClick={onContinue}/>
-    //           </>}
-    //   >
-    //   </Footer>
-      
-    // </>
 
     <>
     <div className={styles.home}>
