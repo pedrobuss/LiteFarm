be383f463e3d6d8f8a3ed91fa93e7045cb4b2166
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-12-02 22:18:26 -0800

    Front end validation

--- packages/webapp/src/components/CustomSignUp/index.js
+++ packages/webapp/src/components/CustomSignUp/index.js
@@ -5,37 +5,57 @@ import Button from '../Form/Button';
 import Input from '../Form/Input';
 import React from 'react';
 import Footer from '../Footer';
+import { actions, Control, Form } from 'react-redux-form';
 
-export default function PureCustomSignUp({ ssoSignUp, onContinue }) {
+export default function PureCustomSignUp({ inputs = [{}], ssoSignUp, onSubmit, disabled }) {
+
+  var color = '';
+  disabled ? color = '#D4DAE3' : color = '#FCE38D';
 
   return (
 
     <>
-    <div className={styles.home}>
-      <div className={styles.lander}>
+    <div className={styles.home} >
+      <div className={styles.lander} >
         <div className={styles.greetContainer}>
           <img src={Logo}/>
           <div className={styles.ssoButton}>
-          <Button fullLength children='CONTINUE WITH GOOGLE' color='secondary' onClick={ssoSignUp} />
+          <Button style={{width: 312, height: 48}} fullLength children='CONTINUE WITH GOOGLE' color='secondary' onClick={ssoSignUp} />
           </div>
-          <div className={styles.lineBreak}>
+          <div className={styles.lineBreak} onClick={onSubmit}>
           <img src={LineBreak}/>
           </div>
+           
           <div className={styles.continueButton}>
-          <Input className={styles.manualInputContainer} label='Enter your email' />
+          <Input style={{width: 312}} className={styles.manualInputContainer} {...inputs[0]}/>
           </div>
-        </div>
+          </div>
+          
+        
 
       </div>
+      {/* <div className={styles.bottom} onClick={onSubmit}/> */}
 
     </div>
-    <Footer style={{ position: 'sticky', bottom: '0' }}
+    <Footer style={{ position: 'sticky', bottom: '0'}}
             children={<>
-
-              <Button fullLength children='Continue' onClick={onContinue}/>
+              <div className={styles.bottomButton}>
+              <Button style={{background: color, border: '4px', shadow: '0px 2px 8px rgba(102, 115, 138, 0.3)', width: 312, height: 48}} type='submit' fullLength children='Continue' onClick={onSubmit}/>
+              </div>
             </>}
+
     >
     </Footer>
   </>
+  
+  
   )
 }
+
+// border: none;
+//     color: var(--inputDisabled);
+//     background-color: var(--btnDisabled);
+//     box-shadow: none;
+
+
+
