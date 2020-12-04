8699d80fb775b7ca5f13e4fea4cdfeccb53c1d47
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-12-04 10:32:03 -0800

    API tests

--- packages/api/src/routes/loginRoute.js
+++ packages/api/src/routes/loginRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const loginController = require('../controllers/loginController');
 
 router.post('/', loginController.authenticateUser());
+router.get('/user/:email', loginController.getUserNameByUserEmail());
 
 module.exports = router;
